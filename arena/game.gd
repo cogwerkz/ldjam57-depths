7d1c6d428b7bbf4ea7b514afbf4ae1b436a977e8
Domagoj Cerjan <domagoj.cerjan@gmail.com>
2025-04-06 02:11:23 +0200

    Interpolate ambient lighting with player depth

--- arena/game.gd
+++ arena/game.gd
@@ -1,6 +1,12 @@
 extends Node3D
 
+@onready var player_submarine = $Player/PlayerSubmarine
 @onready var skill_tree_panel = %SkillTreePanel
+
+@export var sky_map: GradientTexture1D
+@export var depth_map: GradientTexture1D
+@export var environment: WorldEnvironment
+@export var sun: DirectionalLight3D
 
 func _input(event: InputEvent) -> void:
 	if event.is_action_pressed("ui_cancel"):
@@ -16,3 +22,17 @@ func _on_skill_tree_panel_visibility_changed() -> void:
 	else:
 		get_tree().paused = false
 		Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+
+func _process(_delta: float) -> void:
+	var depth = player_submarine.global_position.y
+	var t = absf(clampf(depth, -100.0, 0.0)) / 100.0
+	var depth_color = depth_map.gradient.sample(t)
+	var sky_color = sky_map.gradient.sample(t)
+	
+	var sky_material = (environment.environment.sky.sky_material as ProceduralSkyMaterial)
+	sky_material.ground_bottom_color = depth_color
+	sky_material.ground_horizon_color = depth_color
+	sky_material.sky_top_color = sky_color
+	sky_material.sky_horizon_color = depth_color
+	sun.light_energy = 1.0 - t
+	
--- entity/player/player_submarine.gd
+++ entity/player/player_submarine.gd
@@ -78,33 +78,24 @@ func _physics_process(delta: float) -> void:
 
 	var acceleration := current_state.linear_acceleration * 60.0 * delta
 	var overdrive = 1.0
-	var should_auto_level = true
 	if Input.is_action_pressed('throttle_overdrive'):
 		overdrive = 4.0
 	if Input.is_action_pressed('throttle_forward'):
 		apply_force(transform.basis.z * -acceleration * overdrive, Vector3.ZERO)
-		should_auto_level = false
 	if Input.is_action_pressed('throttle_backward'):
 		apply_force(transform.basis.z * acceleration * overdrive, Vector3.ZERO)
-		should_auto_level = false
 	if Input.is_action_pressed('throttle_up'):
 		apply_force(transform.basis.y * acceleration * overdrive, Vector3.ZERO)
-		should_auto_level = false
 	if Input.is_action_pressed('throttle_down'):
 		apply_force(transform.basis.y * -acceleration * overdrive, Vector3.ZERO)
-		should_auto_level = false
 	if Input.is_action_pressed('throttle_left'):
 		apply_force(transform.basis.x * -acceleration * overdrive, Vector3.ZERO)
-		should_auto_level = false
 	if Input.is_action_pressed('throttle_right'):
 		apply_force(transform.basis.x * acceleration * overdrive, Vector3.ZERO)
-		should_auto_level = false
 	if Input.is_action_pressed('roll_left'):
 		apply_torque(transform.basis.z * acceleration * overdrive * 0.2)
-		should_auto_level = false
 	if Input.is_action_pressed('roll_right'):
 		apply_torque(transform.basis.z * -acceleration * overdrive * 0.2)
-		should_auto_level = false
 		
 	if Input.is_action_just_released("tertiary_action"):
 		scan()
@@ -115,20 +106,15 @@ func _physics_process(delta: float) -> void:
 	apply_torque(transform.basis.y * (-yaw))
 	apply_torque(transform.basis.x * (-pitch))
 	
-	if absf(yaw) > 0.5 || absf(pitch) > 0.5:
-		should_auto_level = false 
-	
-	
 	update_gui()
 
 	mouse_deltas *= 0.5
 	
-	if should_auto_level:
-		# Always point Y axis up - "auto-leveling"
-		var new_x = -global_transform.basis.z.cross(Vector3.UP).normalized()
-		var new_z = new_x.cross(Vector3.UP).normalized()
-		var new_basis = Basis(new_x, Vector3.UP, new_z)
-		global_transform.basis = global_transform.basis.slerp(new_basis, 0.8 * delta)
+	# Always point Y axis up - "auto-leveling"
+	var new_x = -global_transform.basis.z.cross(Vector3.UP).normalized()
+	var new_z = new_x.cross(Vector3.UP).normalized()
+	var new_basis = Basis(new_x, Vector3.UP, new_z)
+	global_transform.basis = global_transform.basis.slerp(new_basis, 0.8 * delta)
 
 func on_pickup(area: Area3D):
 	if area is Pickup:
