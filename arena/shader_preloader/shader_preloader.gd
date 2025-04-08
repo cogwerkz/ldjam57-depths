9ed041e5cf36e48638ccaf8453579c142b0aee4b
Domagoj Cerjan <domagoj.cerjan@gmail.com>
2025-04-08 18:56:18 +0200

    Extend compilation time wait

--- arena/shader_preloader/shader_preloader.gd
+++ arena/shader_preloader/shader_preloader.gd
@@ -1,5 +1,5 @@
 extends Node3D
 
 func _ready() -> void:
-	await get_tree().create_timer(1.0).timeout
+	await get_tree().create_timer(3.0).timeout
 	get_tree().change_scene_to_file("res://arena/main_menu/MainMenu.tscn")
